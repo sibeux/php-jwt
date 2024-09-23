1adde6a1c089224b852c2856f653a8bf9ac3f3f8
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-09-24 02:36:00 +0700

    Signed-off-by: Nasrul Wahabi <wahabinasrul@gmail.com>

--- login.php
+++ login.php
@@ -14,26 +14,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
 
     // Cek pengguna di database
-    $stmt = $conn->prepare("SELECT * FROM user WHERE email_user = :email");
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($stmt = $conn->prepare("SELECT * FROM user WHERE email_user = :email")){
+        
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($user && password_verify($password, $user['pass_user'])) {
-        $payload = [
-            'iat' => $issuedAt,
-            'exp' => $expirationTime,
-            'iss' => $issuer,
-            'data' => [
-                'id' => $user['id_user'],
-                'email' => $user['email_user'],
-            ],
-        ];
+        if ($user && password_verify($password, $user['pass_user'])) {
+            $payload = [
+                'iat' => $issuedAt,
+                'exp' => $expirationTime,
+                'iss' => $issuer,
+                'data' => [
+                    'id' => $user['id_user'],
+                    'email' => $user['email_user'],
+                ],
+            ];
 
-        $jwt = JWT::encode($payload, $key, 'HS256');
-        echo json_encode(['token' => $jwt]);
+            $jwt = JWT::encode($payload, $key, 'HS256');
+            echo json_encode(['token' => $jwt]);
+        } else {
+            http_response_code(401);
+            echo json_encode(['message' => 'Invalid credentials']);
+        }
     } else {
-        http_response_code(401);
-        echo json_encode(['message' => 'Invalid credentials']);
+        http_response_code(500);
+        echo json_encode(['message' => 'Internal server error']);
     }
+} else {
+    http_response_code(405);
+    echo json_encode(['message' => 'Method not allowed']);
 }
